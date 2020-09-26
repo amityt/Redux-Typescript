f18aa3f09be06e0ce4488379dfbced73670b8a6a
Amit Kumar Das <amitkumar.das@mayadata.io>
2020-09-26 18:56:19 +0530

    Minor change
    Signed-off-by: Amit Kumar Das <amitkumar.das@mayadata.io>

--- cypress/components/ProgressBar.spec.tsx
+++ cypress/components/ProgressBar.spec.tsx
@@ -3,7 +3,7 @@ import mount from "cypress-react-unit-test";
 import React from "react";
 import LinearProgressBar from "../../src/components/linearProgressBar";
 
-//Demo Test 15
+//Demo Test 16
 describe("LinearProgressBar", () => {
 	it("ProgressBar mount", () => {
 		mount(<LinearProgressBar value={6} />);
